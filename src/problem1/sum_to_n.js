e16dbe80aa5c48c26aa43ed60a1da1ec86611396
phuonganh090186 <phuonganh090186@gmail.com>
2023-09-12 08:45:50 +0700

    problem1 challenge

--- src/problem1/sum_to_n.js
+++ src/problem1/sum_to_n.js
@@ -1,11 +1,19 @@
-var sum_to_n_a = function(n) {
-    // your code here
+var sum_to_n_a = function (n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
 };
 
-var sum_to_n_b = function(n) {
-    // your code here
+var sum_to_n_b = function (n) {
+  return (n * (n + 1)) / 2;
 };
 
-var sum_to_n_c = function(n) {
-    // your code here
+var sum_to_n_c = function (n) {
+  let numberArray = Array.from(Array(n), (_, i) => i + 1);
+  return numberArray.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 };
